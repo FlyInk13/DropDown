3391bd6217b4119568521aa3688258c04b42a909
FlyInk <github@k-94.ru>
2018-03-29 01:53:49 +0300

    ie10 find fix

--- source/utils.js
+++ source/utils.js
@@ -123,6 +123,29 @@ function stopPropagation(event) {
 }
 
 
+if (!Array.prototype.find) {
+    Array.prototype.find = function find(predicate) {
+        if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+                return value;
+            }
+        }
+        return undefined;
+    };
+}
+
 if (!Element.prototype.scrollIntoViewIfNeeded) {
     Element.prototype.scrollIntoViewIfNeeded = function scrollIntoViewIfNeeded(centerIfNeeded) {
         centerIfNeeded = arguments.length === 0 ? true : !!centerIfNeeded;
